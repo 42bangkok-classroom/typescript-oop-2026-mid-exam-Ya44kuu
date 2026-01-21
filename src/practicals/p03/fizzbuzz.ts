6a5b3d9e6b7385518c96e8061b5d97c771fa7750
Ya44kuu <mr.sirisart1999@gmail.com>
2026-01-21 15:41:56 +0700

    8

--- src/practicals/p03/fizzbuzz.ts
+++ src/practicals/p03/fizzbuzz.ts
@@ -3,20 +3,24 @@ export{};
 let args = process.argv.slice(2);
 let input = Number(args[0]);
 
-if(input <=0  || isNaN(input)){
-    // console.log("Invalid input");
-    process.exit(1);
-}
+const fizz = (input:number)=>{
+    
+    if(input <=0  || isNaN(input)){
+        // console.log("Invalid input");
+        process.exit(1);
+    }
 
-for(let i = 1;i<=input;i++){
-    if(i%3 === 0&& i%5 === 0){
-        console.log("FizzBuzz");
-    }else if(i%3===0){
-        console.log("Fizz");
+    for(let i = 1;i<=input;i++){
+        if(i%3 === 0&& i%5 === 0){
+            console.log("FizzBuzz");
+        }else if(i%3===0){
+            console.log("Fizz");
 
-    }else if(i%5 === 0){
-        console.log("Buzz");
-    }else{
-        console.log(String(i));
+        }else if(i%5 === 0){
+            console.log("Buzz");
+        }else{
+            console.log(String(i));
+        }
     }
 }
+fizz(input);
