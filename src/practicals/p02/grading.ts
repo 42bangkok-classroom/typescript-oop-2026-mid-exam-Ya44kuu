fa53db1295aeb18a1bddcec5c28c99b468874f81
Ya44kuu <mr.sirisart1999@gmail.com>
2026-01-21 15:37:42 +0700

    6

--- src/practicals/p02/grading.ts
+++ src/practicals/p02/grading.ts
@@ -2,22 +2,24 @@
 let args = process.argv.slice(2);
 let input = Number(args[0]);
 
-if(input <0 || input >100 || isNaN(input)){
-    console.log("Invalid input");
-    process.exit(1);
-}
-
-if(input  >= 80){
-    console.log("Grade is A");
-}else if(input >= 70){
-    console.log("Grade is B");
-}
-else if(input >= 60){
-    console.log("Grade is C");
-}
-else if(input >= 50){
-    console.log("Grade is D");
-}
-else{
-    console.log("Grade is F");
+const grade = ():void=>{
+    if(input <0 || input >100 || isNaN(input)){
+        console.log("Invalid input");
+        return;
+    }
+    
+    if(input  >= 80){
+        console.log("Grade is A");
+    }else if(input >= 70){
+        console.log("Grade is B");
+    }
+    else if(input >= 60){
+        console.log("Grade is C");
+    }
+    else if(input >= 50){
+        console.log("Grade is D");
+    }
+    else{
+        console.log("Grade is F");
+    }
 }
