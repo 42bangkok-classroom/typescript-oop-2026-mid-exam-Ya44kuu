52310ca893b84d3e5f837d612c0283af7e284097
Ya44kuu <mr.sirisart1999@gmail.com>
2026-01-21 14:17:07 +0700

    1

--- src/practicals/p01/hello.ts
+++ src/practicals/p01/hello.ts
@@ -1,2 +1,2 @@
 // Write your code below
-console.log("hello")
+console.log("Hello Exam");
--- src/practicals/p02/grading.ts
+++ src/practicals/p02/grading.ts
@@ -1,2 +1,23 @@
 // Write your code below
-sfdsfd
+let args = process.argv.slice(2);
+let input = Number(args[0]);
+
+if(input <0 || input >100 || isNaN(input)){
+    console.log("Invalid input");
+    process.exit();
+}
+
+if(input  >= 80){
+    console.log("A");
+}else if(input >= 70){
+    console.log("B");
+}
+else if(input >= 60){
+    console.log("C");
+}
+else if(input >= 50){
+    console.log("D");
+}
+else{
+    console.log("F");
+}
